12fe2ad51f5ff54459ce72f3c555b4ee3abee7eb
Bug-Storm <meteo.tethieu@gmail.com>
2021-02-11 02:05:59 +0100

    Set bêta

---
+++ Produits.php
@@ -0,0 +1,139 @@
+<?php
+include './lire_un.php';
+
+
+class Produits{
+    // Connexion
+    private $connexion;
+    private $table = "archive";
+    private $tableuser = "users";
+
+    // object properties
+    public $dateTime;
+    public $usUnits;
+    public $altimeter;
+    public $appTemp;
+    public $barometer;
+    public $dewpoint;
+    public $heatindex;
+    public $humidex;
+    public $outTemp;
+    public $outHumidity;
+    public $pressure;
+    public $rain;
+    public $rainRate;
+    public $windchill;
+    public $windDir;
+    public $windGust;
+    public $windSpeed;
+    public $windGustDir;
+    public $id;
+    public $apikey;
+    public $apisignature;
+    
+
+    /**
+     * Constructeur avec $db pour la connexion à la base de données
+     *
+     * @param $db
+     */
+    public function __construct($db){
+        $this->connexion = $db;
+    }
+
+
+    public function getuser(){
+        // On écrit la requête
+        $sql = "SELECT id, apikey, apisignature FROM " .$this->tableuser. " LIMIT 1";
+
+        // On prépare la requête
+        $query = $this->connexion->prepare($sql);
+
+        // On attache l'id
+        $query->bindParam(1, $this->id);
+
+        // On exécute la requête
+        $query->execute();
+
+         // on récupère la ligne
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+       
+        // On hydrate l'objet
+    $this->id = $row['id'];
+    $this-> apikey  = $row ['apikey'];
+    $this-> apisignature = $row ['apisignature'];
+
+    }
+
+    /**
+     * Créer un produit
+     *
+     * @return void
+    
+    
+     * Lecture des produits
+     *
+     * @return void
+     */
+    public function lire(){
+        // On écrit la requête
+        $sql = "SELECT dateTime, usUnits, altimeter, appTemp, barometer, dewpoint, heatindex,humidex, outHumidity, outTemp, pressure, rain,rainRate,windchill,windDir,windGust,windGustDir,windSpeed FROM " . $this->table . "";
+
+        // On prépare la requête
+        $query = $this->connexion->prepare($sql);
+        
+
+           // On attache l'id
+           $query->bindParam(1, $this->id);
+
+           // On exécute la requête
+           $query->execute();
+   
+           // on récupère la ligne
+           $row = $query->fetch(PDO::FETCH_ASSOC);
+    }
+
+    
+    /**
+     * Lire un produit
+     *
+     * @return void
+     */
+    public function lireUn(){
+        // On écrit la requête
+        $sql = "SELECT dateTime, usUnits, altimeter, appTemp, barometer, dewpoint, heatindex,humidex, outHumidity, outTemp, pressure, rain,rainRate,windchill,windDir,windGust,windGustDir,windSpeed FROM " . $this->table .  " LIMIT 1";
+
+        // On prépare la requête
+        $query = $this->connexion->prepare( $sql );
+
+        // On attache l'id
+        $query->bindParam(1, $this->dateTime);
+
+        // On exécute la requête
+        $query->execute();
+
+        // on récupère la ligne
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+
+        // On hydrate l'objet
+        $this->dateTime = $row['dateTime'];
+        $this-> usUnits  = $row ['usUnits'];
+        $this-> altimeter = $row ['altimeter'];
+        $this-> appTemp = $row ['appTemp'];
+        $this-> barometer = $row ['barometer'];
+        $this-> dewpoint = $row ['dewpoint'];
+        $this-> heatindex = $row ['heatindex'];
+        $this-> humidex = $row ['humidex'];
+        $this-> outTemp = $row ['outTemp'];
+        $this-> outHumidity = $row ['outHumidity'];
+        $this-> pressure = $row ['pressure'];
+        $this-> rain = $row ['rain'];
+        $this-> rainRate = $row ['rainRate'];
+        $this-> windchill = $row ['windchill'];
+        $this-> windDir = $row ['windDir'];
+        $this-> windGust = $row ['windGust'];
+        $this-> windGustDir = $row ['windGustDir'];
+        $this-> windSpeed = $row['windSpeed'];
+    }
+
+}
---
+++ lire_un.php
@@ -0,0 +1,88 @@
+<?php
+// Headers requis
+header("Access-Control-Allow-Origin: *");
+
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 300");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// On vérifie que la méthode utilisée est correcte
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    // On inclut les fichiers de configuration et d'accès aux données
+    include_once './Database.php';
+    include_once './Produits.php';
+
+    // On instancie la base de données
+    $database = new Database();
+    $db = $database->getConnection();
+
+    // On instancie les produits
+    $produit = new Produits($db);
+
+
+    //Make sure that our query string parameters exist.
+    if (isset($_GET['t']) && isset($_GET['id']) && isset($_GET['apikey']) && isset($_GET['apisignature'])) {
+        $id = trim($_GET['id']); //Get l'id via la query string
+        $apikey = trim($_GET['apikey']); //Get l'api Key via la query string
+        $apisignature = trim($_GET['apisignature']); //Get l'api Signature via la query string
+        $t = trim($_GET['t']); //Get le Time  via la query string
+        $timestampnow = time(); //Timestamp Now
+        $timestamp = $timestampnow - $t; // On calc la difference entre le timestamp now et celui de la query string
+
+
+        if ($timestamp <= 300) { // Si le $timestamp est == ou < a 300s ou 5m la requete est bonne  sinon un erreur 404 est envoyé 
+            $produit->getuser();
+
+            if ($produit->id == $id and $produit->apikey == $apikey and $produit->apisignature == $apisignature) {
+                // On récupère le produit
+                $produit->lireUn();
+
+                // On vérifie si le produit existe
+                if ($produit->dateTime != null) {
+
+                    $prod = [
+                        "datetime" => $produit->dateTime,
+                        "usUnits"  => $produit->usUnits,
+                        "altimeter" => $produit->altimeter,
+                        "appTemp" => $produit->appTemp,
+                        "barometer" => $produit->barometer,
+                        "dewpoint" => $produit->dewpoint,
+                        "heatindex" => $produit->heatindex,
+                        "humidex" => $produit->humidex,
+                        "outTemp" => $produit->outTemp,
+                        "outHumidity" => $produit->outHumidity,
+                        "pressure" => $produit->pressure,
+                        "rain" => $produit->rain,
+                        "rainRate" => $produit->rainRate,
+                        "windchill" => $produit->windchill,
+                        "windDir" => $produit->windDir,
+                        "windGust" => $produit->windGust,
+                        "windGustDir" => $produit->windGustDir,
+                        "windSpeed" => $produit->windSpeed
+                    ];
+                    // On envoie le code réponse 200 OK
+                    http_response_code(200);
+
+                    // On encode en json et on envoie
+                    echo json_encode($prod);
+                } else {
+                    //Token is not valid.
+
+                    // 404 Not found
+                    http_response_code(404);
+
+                    echo json_encode(array("message" => "Vous avez depasser le limit de 24h!."));
+                }
+            } else {
+                // On gère l'erreur
+                http_response_code(405);
+                echo json_encode(["message" => "La méthode n'est pas autorisée"]);
+            }
+        } else {
+            // 404 Not found
+            http_response_code(404);
+
+            echo json_encode(array("message" => "Vous avez dépasse le limit de 5m."));
+        }
+    }
+}
