9221f9a6016a6b4a9a828f959e396a35e96fc839
Bug-Storm <meteo.tethieu@gmail.com>
2021-02-14 20:58:59 +0100

    Mise a jour

--- weewx.php
+++ weewx.php
@@ -1,8 +1,6 @@
 <?php
 
-
-
-class weewx{
+class Weewx{
     // Connexion
     private $connexion;
     private $table = "archive";
@@ -30,6 +28,10 @@ class weewx{
     public $id;
     public $apikey;
     public $apisignature;
+    public $starttimestamp;
+    public $endtimestamp;
+    
+
     
 
     /**
@@ -77,63 +79,21 @@ class weewx{
      */
     public function lire(){
         // On écrit la requête
-        $sql = "SELECT dateTime, usUnits, altimeter, appTemp, barometer, dewpoint, heatindex,humidex, outHumidity, outTemp, pressure, rain,rainRate,windchill,windDir,windGust,windGustDir,windSpeed FROM " . $this->table . "";
-
-        // On prépare la requête
-        $query = $this->connexion->prepare($sql);
-        
-
-           // On attache l'id
-           $query->bindParam(1, $this->id);
-
-           // On exécute la requête
-           $query->execute();
-   
-           // on récupère la ligne
-           $row = $query->fetch(PDO::FETCH_ASSOC);
-    }
-
-    
-    /**
-     * Lire un produit
-     *
-     * @return void
-     */
-    public function lireUn(){
         // On écrit la requête
-        $sql = "SELECT dateTime, usUnits, altimeter, appTemp, barometer, dewpoint, heatindex,humidex, outHumidity, outTemp, pressure, rain,rainRate,windchill,windDir,windGust,windGustDir,windSpeed FROM " . $this->table .  " LIMIT 1";
-
+        $sql = "SELECT * FROM " . $this->table .  " WHERE dateTime BETWEEN :dateTime AND :dateTime1 ";
+        
         // On prépare la requête
         $query = $this->connexion->prepare( $sql );
-
-        // On attache l'id
-        $query->bindParam(1, $this->dateTime);
-
+        
+        // On attache le dateTime vers le Startimestamp  & Endtimestamp
+        $query->bindParam(':dateTime', $this->starttimestamp );
+        $query->bindParam(':dateTime1', $this->endtimestamp );
+   
         // On exécute la requête
         $query->execute();
 
-        // on récupère la ligne
-        $row = $query->fetch(PDO::FETCH_ASSOC);
-
-        // On hydrate l'objet
-        $this->dateTime = $row['dateTime'];
-        $this-> usUnits  = $row ['usUnits'];
-        $this-> altimeter = $row ['altimeter'];
-        $this-> appTemp = $row ['appTemp'];
-        $this-> barometer = $row ['barometer'];
-        $this-> dewpoint = $row ['dewpoint'];
-        $this-> heatindex = $row ['heatindex'];
-        $this-> humidex = $row ['humidex'];
-        $this-> outTemp = $row ['outTemp'];
-        $this-> outHumidity = $row ['outHumidity'];
-        $this-> pressure = $row ['pressure'];
-        $this-> rain = $row ['rain'];
-        $this-> rainRate = $row ['rainRate'];
-        $this-> windchill = $row ['windchill'];
-        $this-> windDir = $row ['windDir'];
-        $this-> windGust = $row ['windGust'];
-        $this-> windGustDir = $row ['windGustDir'];
-        $this-> windSpeed = $row['windSpeed'];
+        return $query;
     }
 
 }
+
